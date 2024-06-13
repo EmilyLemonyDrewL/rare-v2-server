a95792602ab07622f79c9ee91ba4f5eacc9c1ba4
Emily Drew L <DontDenyHighFives@gmail.com>
2024-06-13 17:07:53 -0500

    basic crud for comments and reactions works

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -1,10 +1,14 @@
 from django.db import models
 from .rare_user import RareUser
 from .post import Post
+from django.utils import timezone
 
 class Comment(models.Model):
 
     author = models.ForeignKey(RareUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=140)
-    created_on = models.DateTimeField(auto_now=False)
+    created_on = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.content
